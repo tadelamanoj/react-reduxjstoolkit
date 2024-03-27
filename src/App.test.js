037270a6e4062cba904a021df0db3f7e1016bfcd
tadelamanoj <manojkumartadela333@gmail.com>
2024-03-27 16:02:05 +0530

    reduxjs toolkit and testing prokject 1st commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,33 @@
-import { render, screen } from '@testing-library/react';
+// src/App.test.js
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import store from './app/store';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders counter with initial value', () => {
+  const { getByText } = render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+
+  expect(getByText('Counter: 0')).toBeInTheDocument();
+});
+
+test('increments and decrements counter', () => {
+  const { getByText } = render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+
+  const incrementButton = getByText('Increment');
+  const decrementButton = getByText('Decrement');
+
+  fireEvent.click(incrementButton);
+  expect(getByText('Counter: 1')).toBeInTheDocument();
+
+  fireEvent.click(decrementButton);
+  expect(getByText('Counter: 0')).toBeInTheDocument();
 });
